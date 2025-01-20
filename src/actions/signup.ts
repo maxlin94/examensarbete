e61ce5f67a24e06c07cfd74bedce46acb97f2152
Max Lindström <max941@live.se>
2025-01-20 20:10:24 +0100

    Implement signup and authentication functionality with NextAuth and Prisma

---
+++ src/actions/signup.ts
@@ -0,0 +1,52 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import bcrypt from "bcrypt";
+import signupFormSchema from "@/schemas/signup";
+import { ZodError, ZodIssue } from "zod";
+
+export async function signup(formData: FormData) {
+    try {
+        const user = {
+            name: formData.get("name") as string,
+            email: formData.get("email") as string,
+            password: formData.get("password") as string,
+            lastName: formData.get("lastName") as string,
+        };
+
+        signupFormSchema.parse(user);
+
+        const existingUser = await prisma.user.findUnique({
+            where: { email: user.email },
+        });
+
+        if (existingUser) {
+            return JSON.stringify({ error: { message: "Email already exists", status: 400 } });
+        }
+
+        const hashedPassword = await bcrypt.hash(user.password, 10);
+        const fullName = `${user.name} ${user.lastName}`;
+
+        const success = await prisma.user.create({
+            data: {
+                name: fullName,
+                email: user.email,
+                password: hashedPassword,
+            },
+        });
+        if (success) {
+            return JSON.stringify({ ok: { message: "User created", status: 201 } });
+        } else {
+            return JSON.stringify({ error: { message: "Something went wrong", status: 500 } });
+        }
+    } catch (error) {
+        if (error instanceof ZodError) {
+            const errorMap = new Map<string, string>(
+                error.errors.map((issue: ZodIssue) => [issue.path.join("."), issue.message])
+            );
+            return JSON.stringify({ error: Object.fromEntries(errorMap) });
+        } else {
+            console.log("An unexpected error occured", error);
+        }
+    }
+}
---
+++ src/components/input.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ErrorMessage from "@/components/errorMessage";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+    errorMessage?: string;
+}
+
+export default function Input({
+                                  errorMessage,
+                                  ...props
+                              }: InputProps) {
+    return (
+        <div className="flex flex-col items-center space-y-2">
+            <input
+                className={`px-4 py-2 text-black border border-gray-300 rounded-lg shadow-sm
+                 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 placeholder-gray-400`}
+                {...props}
+            />
+            {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
+        </div>
+    );
+}
