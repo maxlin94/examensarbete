175e2f9618afb0d5cc730b43ec4fac95873ca6ed
Max Lindström <max941@live.se>
2025-01-21 05:32:24 +0100

    Add chat functionality with message saving and real-time updates

---
+++ src/actions/saveMessage.ts
@@ -0,0 +1,35 @@
+"use server";
+
+import prisma from "@/util/prisma";
+import { validateUser } from "@/util/user";
+
+export async function saveMessage(message: MessageType) {
+    const user = await validateUser();
+    if (!user) {
+        return;
+    }
+    const friendship = await prisma.friendShip.findFirst({
+        where: {
+            OR: [
+                {
+                    AND: [{ userId: user.id }, { friendId: message.receiverId }],
+                },
+                {
+                    AND: [{ userId: message.receiverId }, { friendId: user.id }],
+                },
+            ],
+        },
+        select: {
+            id: true,
+        },
+    });
+    if (!friendship) return;
+    return prisma.message.create({
+        data: {
+            content: message.content,
+            senderId: user.id,
+            receiverId: message.receiverId,
+            friendShipId: friendship.id,
+        },
+    });
+}
