bd3ff5eecd3f789a2ce31fd6ffa902b71c82e3d5
Max Lindström <max941@live.se>
2025-01-23 08:53:43 +0100

    Fix

--- global.d.ts
+++ global.d.ts
@@ -1,30 +1,35 @@
-type UserType = {
-    id: string,
-    name: string,
-    email: string,
-    password: string,
-    createdAt: Date,
-    updatedAt: Date,
-    isFriend: boolean,
-    friendRequestSent: boolean
-}
-
-type FriendRequestType = {
-    id: string,
-    user: UserType
-}
-
-type MessageType = {
-    content: string,
-    senderId: string,
-    receiverId: string,
-    friendshipId: string
-}
-
 declare global {
-    type UserType = UserType
-    type FriendRequestType = FriendRequestType
-    type MessageType = MessageType
+    type UserType = {
+        id: string;
+        name: string;
+        email: string;
+        password: string;
+        createdAt: Date;
+        updatedAt: Date;
+    };
+
+    type UserDto = {
+        name: string;
+        id: string;
+        isFriend?: boolean;
+        isFriendRequestSent?: boolean;
+        friendshipId?: string;
+        lastMessage?: MessageType | null;
+    };
+
+    type FriendRequestType = {
+        id: string;
+        user: UserType;
+    };
+
+    type MessageType = {
+        content: string;
+        senderId: string;
+        receiverId: string;
+        friendshipId: string;
+        sentByUser?: boolean;
+        createdAt?: Date;
+    };
 }
 
 export {};
