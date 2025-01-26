c3853a0bf1c7d3dc8a6340a73167e26f768e74f3
Max Lindström <max941@live.se>
2025-01-26 20:54:07 +0100

    Remove unused message fetching logic and socket event handling from ChatBox component

--- src/components/chat/chatBox.tsx
+++ src/components/chat/chatBox.tsx
@@ -1,39 +1,10 @@
-import { useEffect } from 'react';
 import ChatInput from '@/components/chat/chatInput';
-import socket from '@/util/socket';
 import ScrollWrapper from '@/components/shared/scrollWrapper';
 import useStore from '@/store';
 import ChatMessages from './chatMessages';
 
 export default function ChatBox() {
-  const { selectedFriend, addMessage, setMessages } = useStore();
-
-  useEffect(() => {
-    const fetchMessages = async () => {
-      if (!selectedFriend?.friendshipId) return;
-      try {
-        const response = await fetch(`/api/messages/${selectedFriend.id}`);
-        if (!response.ok) throw new Error("Failed to fetch messages");
-
-        const data = await response.json();
-        setMessages(selectedFriend.friendshipId, data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    if (selectedFriend) {
-      fetchMessages();
-      socket.emit("joinRoom", selectedFriend.friendshipId);
-      socket.on("privateMessage", (message: MessageType) => {
-        addMessage(message);
-      });
-    }
-
-    return () => {
-      socket.off("privateMessage");
-    };
-  }, [selectedFriend, setMessages, addMessage]);
+  const { selectedFriend } = useStore();
 
   return (
     <div className="flex flex-col w-3/4 p-4 rounded-md">
