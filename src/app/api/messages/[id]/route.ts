3320c752cc194b77b47c2fcb813f1fe291fa9fd8
Max Lindström <max941@live.se>
2025-01-27 07:20:51 +0100

    Refactor

--- src/app/api/messages/[id]/route.ts
+++ src/app/api/messages/[id]/route.ts
@@ -1,22 +1,29 @@
 import { NextResponse } from "next/server";
 import { NextApiRequest } from "next"
 import prisma from "@/util/prisma";
-import { validateUser } from "@/util/user";
+import { getServerSession } from "next-auth";
 
 export async function GET(req: NextApiRequest, { params }: { params: Promise<{ id: string }> }) {
     try {
-        const user = await validateUser();
+        const session = await getServerSession();
+        if (!session || !session.user) return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
         const { id } = await params;
         const { searchParams } = new URL(req.url || '');
         const limit = parseInt(searchParams.get('limit') || '20');
         const offset = parseInt(searchParams.get('offset') || '0');
 
-        if (!user) return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
         const messages = await prisma.friendship.findFirst({
             where: { id: id
+
             },
             select: {
                 messages: {
+                    where: {
+                        OR: [
+                            { senderId: session.user.id },
+                            { receiverId: session.user.id }
+                        ]
+                    },
                     orderBy: {
                         createdAt: 'desc'
                     },
