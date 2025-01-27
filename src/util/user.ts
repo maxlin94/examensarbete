c62c7b59d33b04718e43beca05cbd1b62e6d5c16
Max Lindström <max941@live.se>
2025-01-27 07:20:31 +0100

    Remove unused getFriendsByUserId function

--- src/util/user.ts
+++ src/util/user.ts
@@ -51,32 +51,6 @@ export async function getUsersByName(name: string, id: string): Promise<UserWith
     });
 }
 
-export async function getFriendsByUserId(id: string): Promise<FriendType[]> {
-    const friendships = await prisma.friendship.findMany({
-        where: {
-            OR: [
-                { userId: id },
-                { friendId: id },
-            ],
-        },
-        include: {
-            user: true,
-            friend: true,
-        },
-    });
-
-    return friendships.map((friendship) => {
-        const isUserInFriendship = friendship.userId === id;
-        const friend = isUserInFriendship ? friendship.friend : friendship.user;
-
-        return {
-            id: friend.id,
-            name: friend.name,
-            friendshipId: friendship.id,
-        };
-    });
-}
-
 export async function validateUser() {
     const session = await getServerSession();
     if (!session || !session.user || !session.user.email) {
